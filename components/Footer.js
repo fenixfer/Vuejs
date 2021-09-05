fe646bbbcd3d8bef3532562a8c7d275c4274c1b7
Fernando Gonzalez <frshion8@gmail.com>
2021-09-05 15:02:29 -0500

    Pasando props

--- components/Footer.js
+++ components/Footer.js
@@ -5,18 +5,27 @@
 //! NOTA: No se peude utilizar camelCase en el nombre del componente porque no lo reconoce
 // !Es mejor todo en minusculas y si son 2 palabras separado con un guión
 app.component('footer-banco',{
+    //TODO: Casi siempre se colocan al principio los props
+    // ! Para props se utilizan corchetes
+    // la cantidad declarada en index.html cantidad = "700" aqui se captura en un array
+    props: ['valor','fecha'],
     //El comando /*html*/ hace que pinte el código de html ocupando la extensón es6-string-html
     // se utilizan comillas invertidas `` para los templates
     template: /*html*/`
     <div class="bg-dark py-3 mt-2 text-white">
     <!-- No utilizamos this.texto porque es un código de html aqui en componentes -->
-            <h3>{{texto}} - {{cantidad}}</h3>
+            <h3>{{texto}} - {{valor}}</h3>
+            <p>{{fecha}}</p>
     </div>
     `,
     data() {
         return {
             texto: 'Footer de mi sitio web en componentes',
-            cantidad: 1000
+            
+            // cantidad: 1000 esta la pasamos por props
         }
     },
 })
+
+//TODO: CUIDADO AL UTILIZAR LOS COMPONENES CON EL CDN SI PONEMOS ESTE COMPONENTE AL INICIO DE LA PÁGINA SE PIERDE LO DEMÁS
+//TODO: CON LOS COMPONENTES ES MEJOR UTILIZAR CLI ES MUCHO MEJOR
